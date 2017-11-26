614b5d0301f1119312513fc4026e8e8e57750437
Tyler Tarsi <tylertarsi@college.harvard.edu>
2017-11-26 20:28:03 +0000

    Edited HTML templates to extend layout.html

--- application.py
+++ application.py
@@ -32,6 +32,14 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about", methods=["GET"])
+def about():
+    return render_template("about.html")
+
+@app.route("/team", methods=["GET"])
+def team():
+    return render_template("team.html")
+
 def errorhandler(e):
     """Handle error"""
     return apology(e.name, e.code)
