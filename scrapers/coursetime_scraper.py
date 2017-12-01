fb8c24475adacf334d833d8827ac891bfa324d3a
Aaron Alexander Kruk <aakruk@college.harvard.edu>
2017-12-01 06:03:38 +0000

    Updated scrapers

--- scrapers/coursetime_scraper.py
+++ scrapers/coursetime_scraper.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 
+departments = []
 courses = []
+times = []
 
 # Loop through each page range on course catalog
 for start in range(0, 1400, 100):
@@ -14,8 +16,45 @@ for start in range(0, 1400, 100):
     soup = BeautifulSoup(page, "html.parser")
 
     # Get course name and time
-    courses = soup.find_all(id="srl_title")
+    courses_html = soup.find_all(id="srl_title")
+    times_html = soup.find_all(class_="time")
+
+    # Set trackers for courses that don't have times
+    marker = 0
+    timeless_courses = []
 
     # Parse courses and times into an array
-    for course in courses:
-        courses.append(course.string)
+    for time in times_html:
+        time = time.string
+
+        # Check to see if course has a time
+        if time[0].isalpha() == False:
+            times.append(time)
+            marker += 1
+
+        else:
+            timeless_courses.append(marker)
+            marker += 1
+
+    marker = 0
+
+    for course in courses_html:
+        if marker not in timeless_courses:
+            course = course.string
+
+            # Split scraped course name into course title and department
+            department,course = course.split("-", 1)
+            department = department.rsplit()[0]
+
+            courses.append(course)
+            departments.append(department)
+            marker += 1
+
+        else:
+            marker += 1
+data = {}
+lst = []
+
+for x in range(0,1067):
+    data[str(('Name', "Department", "Time"))] = courses[x], departments[x], times[x]
+
