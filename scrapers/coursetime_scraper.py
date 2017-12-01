e31c1e102a86cb96c3e1119a704aee0131276eab
Tyler Tarsi <tylertarsi@college.harvard.edu>
2017-12-01 02:15:51 +0000

    Updated scraper

--- scrapers/coursetime_scraper.py
+++ scrapers/coursetime_scraper.py
@@ -1,19 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
 
-page_url = "https://courses.harvard.edu/search?fq_coordinated_semester_yr=coordinated_semester_yr%3A%22Sep+to+Dec+2016+%28Fall+Term%29%22&fq_school_nm=school_nm%3A%22Faculty+of+Arts+and+Sciences%22&q=&sort=course_title+asc&start=0&submit=Search"
-
-# Get page html
-result = requests.get(page_url)
-page = result.content
-
-# Parse html using Beautiful Soup 4
-soup = BeautifulSoup(page, "html.parser")
-
-# Get course html
-courses_html = soup.find_all(id="srl_title")
-
-# Parse courses into an array
 courses = []
-for course in courses_html:
-    courses.append(course.string)
+# Loop through each page range
+for start in range(0, 1400, 100):
+    page_url = "https://courses.harvard.edu/search?fq_credit_level=credit_level%3A%22Undergraduate%22&fq_school_nm=school_nm%3A%22Faculty%20of%20Arts%20and%20Sciences%22&fq_term_desc=term_desc%3A%22Fall%202016-2017%22&q=&sort=course_title%20asc&start={}&rows=100".format(start)
+
+    result = requests.get(page_url)
+    page = result.content
+
+    # Parse html using Beautiful Soup 4
+    soup = BeautifulSoup(page, "html.parser")
+
+    # Get course html
+    courses_html = soup.find_all(id="srl_title")
+
+    # Parse courses into an array
+    for course in courses_html:
+        courses.append(course.string)
+
