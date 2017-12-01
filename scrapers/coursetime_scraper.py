a92cd0e7abc75f30ab23832b55dd29b776845d47
Aaron Alexander Kruk <aakruk@college.harvard.edu>
2017-12-01 06:01:44 +0000

    Degree Data name change

--- scrapers/coursetime_scraper.py
+++ scrapers/coursetime_scraper.py
@@ -2,7 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 
 courses = []
-# Loop through each page range
+
+# Loop through each page range on course catalog
 for start in range(0, 1400, 100):
     page_url = "https://courses.harvard.edu/search?fq_credit_level=credit_level%3A%22Undergraduate%22&fq_school_nm=school_nm%3A%22Faculty%20of%20Arts%20and%20Sciences%22&fq_term_desc=term_desc%3A%22Fall%202016-2017%22&q=&sort=course_title%20asc&start={}&rows=100".format(start)
 
@@ -12,10 +13,9 @@ for start in range(0, 1400, 100):
     # Parse html using Beautiful Soup 4
     soup = BeautifulSoup(page, "html.parser")
 
-    # Get course html
-    courses_html = soup.find_all(id="srl_title")
+    # Get course name and time
+    courses = soup.find_all(id="srl_title")
 
-    # Parse courses into an array
-    for course in courses_html:
+    # Parse courses and times into an array
+    for course in courses:
         courses.append(course.string)
-
